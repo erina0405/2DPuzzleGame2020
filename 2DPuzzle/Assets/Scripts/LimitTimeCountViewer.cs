9b4268e9718b56d0ae1e6d1076e1ef8701316632
erina0405 <erina.n0125@gmail.com>
2020-09-13 16:28:32 +0900

    0913

---
+++ 2DPuzzle/Assets/Scripts/LimitTimeCountViewer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LimitTimeCountViewer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI m_TimeText = null;
+
+    private float m_limitTime = 60;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //1秒に1秒ずつ減らしていく
+        m_limitTime -= Time.deltaTime;
+        //マイナスは表示しない
+        if (m_limitTime < 0) m_limitTime = 0;
+        m_TimeText.text = $"{m_limitTime}";
+    }
+}
---
+++ 2DPuzzle/Assets/Scripts/OrbController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Linq;
+
+public class OrbController : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerUpHandler
+{
+    private SpriteRenderer m_spriteRenderer = null;
+
+    public ComboCounter comboCounter = null;
+
+
+    public enum OrbType
+    {
+        Invalide =-1,
+        BlueOrb,
+        GreenOrb,
+        RedOrb,
+        YellowOrb,
+    }
+
+    public OrbType thisOrbType = OrbType. Invalide;
+
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        comboCounter.AddCombo(this.gameObject);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (comboCounter.DragObjList.Count.Equals(0))
+        {
+            return;
+        }
+
+        if (comboCounter.DragObjList.Contains(this.gameObject))
+        {
+            if (comboCounter.DragObjList.Count.Equals(1))
+            {
+                return;
+            }
+
+            comboCounter.MinusCombo();
+            return;
+        }
+
+        if (comboCounter.DragObjList.FirstOrDefault().GetComponent<OrbController>().thisOrbType != thisOrbType)
+        {
+            return;
+        }
+
+
+        comboCounter.AddCombo(this.gameObject);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (comboCounter.ComboCount > 2)
+        {
+            foreach (var orb in comboCounter.DragObjList)
+            {
+                orb.SetActive(false);
+            }
+        }
+        comboCounter.ClearCombo();
+    }
+}
+
+
